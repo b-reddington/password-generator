9e01dfc71935ec17e8d1e7b30bfa5d8d8bec3b9a
b-reddington <brandon.reddington98@gmail.com>
2023-03-12 15:53:20 -0700

    added a loop to generate lowercase letters

--- script.js
+++ script.js
@@ -2,24 +2,33 @@
 // alert, prompt, confirm
 
 // Assignment code here
-var letterString = "abcdefghijklmnopqrstuvwxyz";
-var numberString = "1234567890";
-var specialCharString = "!?.,<>@#$%^&*()-=_+";
-// Splits the variables above into an array
-var letters = letterString.split("");
-var numbers = numberString.split("");
-var specialChar = specialCharString.split("");
-var password = "";
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
+  var characters = {
+    lower: "abcdefghijklmnopqrstuvwxyz",
+    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    special: "!@#$%^&*()_+=-`~?><,./;:'\|[] {}"
+  };
+  // Create a prompt to get the length of the password
+  var passLength = prompt("How many Characters would you like your password to contain? \nMinimum: 8 \nMaximum: 128");
+  var password = "";
+  var lowerCase = function () {
+    for (let i = 0; i < passLength; i++) {
+      password += characters.lower.charAt(Math.floor(Math.random() * characters.lower.length)) 
+    }
+    return password.trim();
+  }
 
+  passwordText.textContent = lowerCase();
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
