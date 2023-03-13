84e3d30df80909a1b807aa8ecc0685c7bb242568
b-reddington <brandon.reddington98@gmail.com>
2023-03-12 22:23:35 -0700

    Added checks for types of characters included in password

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-// if symbols, numbers, and/or capital letters are needed
 // alert, prompt, confirm
-
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
+
+  // Create a prompt to get the length of the password
+  var passLength = prompt("How many Characters would you like your password to contain? \nMinimum: 8 \nMaximum: 128");
+  var lowerCase = confirm("Would you like your password to contain lowercase letters?");
+  var upperCase = confirm("Would you like your password to contain uppercase letters?");
+  var number = confirm("Would you like your password to contain numbers?");
+  var symbols = confirm("Would you like your password to contain symbols?");
   var characters = {
     lower: "abcdefghijklmnopqrstuvwxyz",
     upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers: "0123456789",
     special: "!@#$%^&*()_+=-`~?><,./;:'\|[] {}"
   };
-  // Create a prompt to get the length of the password
-  var passLength = prompt("How many Characters would you like your password to contain? \nMinimum: 8 \nMaximum: 128");
-  var password = "";
-  var lowerCase = function () {
-    for (let i = 0; i < passLength; i++) {
-      password += characters.lower.charAt(Math.floor(Math.random() * characters.lower.length)) 
-    }
-    return password.trim();
+  var types = "";
+
+  if (lowerCase) {
+    types += characters.lower;
+  }
+  if (upperCase) {
+    types += characters.upper;
+  }
+  if (numbers) {
+    types += characters.numbers;
+  }
+  if (special) {
+    types += characters.special;
+  }
   }
 
-  passwordText.textContent = lowerCase();
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
