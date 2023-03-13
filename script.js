f9e57b080846bed5b1c5581c5ee7e6ea78dc2ca9
b-reddington <brandon.reddington98@gmail.com>
2023-03-12 23:23:16 -0700

    Changed var to let, fixed a bug where spaces were added in password, updated variable name passLength to size

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-// alert, prompt, confirm
 // Assignment code here
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var passwordText = document.querySelector("#password");
+  let passwordText = document.querySelector("#password");
 
   // Create a prompt to get the length of the password
-  var passLength = prompt("How many Characters would you like your password to contain? \nMinimum: 8 \nMaximum: 128");
-  var lowerCase = confirm("Would you like your password to contain lowercase letters?");
-  var upperCase = confirm("Would you like your password to contain uppercase letters?");
-  var number = confirm("Would you like your password to contain numbers?");
-  var symbols = confirm("Would you like your password to contain symbols?");
-  var characters = {
-    lower: "abcdefghijklmnopqrstuvwxyz",
-    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    special: "!@#$%^&*()_+=-`~?><,./;:'\|[] {}"
-  };
-  var types = "";
+  let size = prompt("How many Characters would you like your password to contain? \nMinimum: 8 \nMaximum: 128");
+  if (size < 8 || size > 128) {
+    // if password does not meet the requirements, alert an error.
+    alert("Error\nLength does not meet the requirements.\nPlease enter a number between 8 and 128.");
+  } else {
+    // Prompts the user to see if they would like the following characters inside their password
+    let lowerCase = confirm("Would you like your password to contain lowercase letters?");
+    let upperCase = confirm("Would you like your password to contain uppercase letters?");
+    let numbers = confirm("Would you like your password to contain numbers?");
+    let special = confirm("Would you like your password to contain symbols?");
 
-  if (lowerCase) {
-    types += characters.lower;
+    // Stored objects to grab characters from
+    let characters = {
+      lower: "abcdefghijklmnopqrstuvwxyz",
+      upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      numbers: "0123456789",
+      special: "!@#$%^&*()_+=-`~?><,./;:'\|[]{}"
+    };
+    // a letiable to store types with boolean = true
+    let types = "";
+    // If user selects ok on the following prompts, insert type into types letiable
+    if (lowerCase) {
+      types += characters.lower;
+    }
+    if (upperCase) {
+      types += characters.upper;
+    }
+    if (numbers) {
+      types += characters.numbers;
+    }
+    if (special) {
+      types += characters.special;
+    }
+    // Empty password letiable to add random characters in to
+    let password = "";
+    for (let i = 0; i < size; i++) {
+      // Creates a temporary letiable to store the selected characters
+      let generate = Math.floor(Math.random() * types.length);
+      password += types[generate];
+    }
+    passwordText.textContent = password.trim();
   }
-  if (upperCase) {
-    types += characters.upper;
-  }
-  if (numbers) {
-    types += characters.numbers;
-  }
-  if (special) {
-    types += characters.special;
-  }
-  }
-
-
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
